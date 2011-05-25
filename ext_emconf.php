1e8179a32e2e9abaee3ed8b23b3a4d0d37de1fca
z-mark <z-mark@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-25 12:21:54 +0000

    after exdeval, now really
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mzvoting/trunk@48124 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mzvoting".
 #
-# Auto generated 19-05-2011 15:15
+# Auto generated 20-05-2011 13:13
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -32,15 +32,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '4.3-4.5.2',
-			'static_info_tables' => '2.2.0-'
+			'cms' => '',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"4c23";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"774c";s:14:"ext_tables.php";s:4:"d7d3";s:14:"ext_tables.sql";s:4:"5705";s:28:"icon_tx_mzvoting_ranking.gif";s:4:"475a";s:30:"icon_tx_mzvoting_recomends.gif";s:4:"475a";s:26:"icon_tx_mzvoting_votes.gif";s:4:"475a";s:28:"icon_tx_mzvoting_votings.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"30b5";s:7:"tca.php";s:4:"5e19";s:19:"doc/wizard_form.dat";s:4:"536a";s:20:"doc/wizard_form.html";s:4:"bc88";s:29:"pi1/class.tx_mzvoting_pi1.php";s:4:"19d0";s:17:"pi1/locallang.xml";s:4:"9512";s:29:"pi2/class.tx_mzvoting_pi2.php";s:4:"5e16";s:17:"pi2/locallang.xml";s:4:"523e";s:37:"static/static_templates/constants.txt";s:4:"bdd3";s:33:"static/static_templates/setup.txt";s:4:"59b5";}',
+	'_md5_values_when_last_written' => 'a:26:{s:9:"ChangeLog";s:4:"a456";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"2f08";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"774c";s:14:"ext_tables.php";s:4:"c7cd";s:14:"ext_tables.sql";s:4:"8b44";s:28:"icon_tx_mzvoting_ranking.gif";s:4:"475a";s:30:"icon_tx_mzvoting_recomends.gif";s:4:"475a";s:26:"icon_tx_mzvoting_votes.gif";s:4:"475a";s:28:"icon_tx_mzvoting_votings.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"5946";s:7:"tca.php";s:4:"8c03";s:19:"doc/wizard_form.dat";s:4:"2556";s:20:"doc/wizard_form.html";s:4:"d923";s:29:"pi1/class.tx_mzvoting_pi1.php";s:4:"19d0";s:17:"pi1/locallang.xml";s:4:"9512";s:29:"pi2/class.tx_mzvoting_pi2.php";s:4:"5e16";s:17:"pi2/locallang.xml";s:4:"523e";s:35:"res/infofiles/set_terms_params.html";s:4:"1dad";s:37:"static/static_templates/constants.txt";s:4:"0ec6";s:33:"static/static_templates/setup.txt";s:4:"8b4f";s:36:"static/templates/pi1_votingform.html";s:4:"a5a1";s:33:"static/templates/pi2_ranking.html";s:4:"f40d";s:34:"static/voting_system/constants.txt";s:4:"bdd3";s:30:"static/voting_system/setup.txt";s:4:"59b5";}',
 );
 
 ?>
--- pi1/class.tx_mzvoting_pi1.php
+++ pi1/class.tx_mzvoting_pi1.php
@@ -24,7 +24,26 @@
 /**
  * [CLASS/FUNCTION INDEX of SCRIPT]
  *
- * Hint: use extdeveval to insert/update function index above.
+ *
+ *
+ *   59: class tx_mzvoting_pi1 extends tslib_pibase
+ *   72:     function main($content, $conf)
+ *  117:     function form_handler()
+ *  152:     function get_votingform()
+ *  178:     function render_terms()
+ *  208:     function render_countries()
+ *  254:     function formdata_handler($formdata)
+ *  402:     function check_for_existing_votes($mail)
+ *  423:     function send_confirmation_mail($mail, $firstname, $lastname, $vid, $secret )
+ *  488:     function confirmation_handler()
+ *  580:     function report_error($errorcode)
+ *  595:     function load_config($conf)
+ *  653:     function show_button()
+ *  681:     function test()
+ *
+ * TOTAL FUNCTIONS: 13
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
  */
 
 require_once(PATH_tslib.'class.tslib_pibase.php');
@@ -42,119 +61,119 @@ class tx_mzvoting_pi1 extends tslib_pibase {
 	var $scriptRelPath = 'pi1/class.tx_mzvoting_pi1.php';	// Path to this script relative to the extension dir.
 	var $extKey        = 'mzvoting';	// The extension key.
 	var $pi_checkCHash = true;
-	
+
 	/**
 	 * The main method of the PlugIn
 	 *
 	 * @param	string		$content: The PlugIn content
 	 * @param	array		$conf: The PlugIn configuration
-	 * @return	The content that is displayed on the website
+	 * @return	The		content that is displayed on the website
 	 */
 	function main($content, $conf) {
 		//$this->conf = $conf;
 		//$this->pi_setPiVarDefaults();
-		
+
 		$this->load_config($conf);
-		
+
 		$this->pi_loadLL();
-		
+
 		$this->getVars = t3lib_div::_GET('tx_mzvoting');
-		
-		
-		
+
+
+
 		// Get the template
 		$this->templateHtml = $this->cObj->fileResource($this->conf['templateFile']);
-	
+
 		/**
 		 * What do we have to display?
 		 */
-	
+
 		switch ($this->getVars['action']) {
-			case 2: 
+			case 2:
 				/** confirm a vote **/
 				$content = $this->confirmation_handler();
 				break;
-			case 1:		
+			case 1:
 				/** display the form **/
 				$content = $this->form_handler();
 				break;
-			default: 
+			default:
 				/** display the button **/
 				$content = $this->show_button();
 				break;
 		}
-	
-	
+
+
 		return $this->pi_wrapInBaseClass($content);
-	}	
-	
-	
-	
+	}
+
+
+
 	/**
 	 * decides if we have to display the form or have to handel any data
 	 *
-	 * @return	html output
+	 * @return	html		output
 	 */
 	function form_handler() {
 		if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 			t3lib_div::debug();
 		}
-		
+
 		/**
 		* Have we got any form data
 		**/
 		if(isset($_POST['tx_mzvoting_pi1_votingform'])) {
-			/** 
+			/**
 			* do something with the data
 			**/
-			
+
 			$return = $this->formdata_handler($_POST['tx_mzvoting_pi1_votingform']);
-			
+
 		} else {
-			/** 
+			/**
 			* get the form
 			**/
-			
+
 			$return = $this->get_votingform();
 		}
-		
-		
+
+
 	return($return);
-	
-	}	
-	
-	
-	
+
+	}
+
+
+
 	/**
 	 * renders the form
 	 *
-	 * @return	the voting form
+	 * @return	the		voting form
 	 */
 	function get_votingform() {
-		
+
 		// Extract subparts from the template
 		$subpart = $this->cObj->getSubpart($this->templateHtml, '###VOTINGFORM###');
 
 		// Fill marker array
 		$markerArray['###ACTION###'] = ''; //[todo] define by typoscript/constants
 		$markerArray['###METHOD###'] = 'post'; //[todo] define by typoscript/constants
-		
-		
-		$markerArray['###COUNTRY###']  =  $this->render_countries(); 
-		$markerArray['###TERMSANDCONDITIONS###']  =  $this->render_terms(); 
-		
+
+
+		$markerArray['###COUNTRY###']  =  $this->render_countries();
+		$markerArray['###TERMSANDCONDITIONS###']  =  $this->render_terms();
+
 		// Create the content by replacing the content markers in the template
 		$return = $this->cObj->substituteMarkerArrayCached($subpart, $markerArray);
-		
+
 		return($return);
-	
+
 	}
-	
-	
+
+
 	/**
 	 * Renders and returns terms andconditions if needet
 	 *
-	 * @return	the html for terms & conditions
+	 * @return	the		html for terms & conditions
 	 */
 	function render_terms() {
 		/**
@@ -163,29 +182,28 @@ class tx_mzvoting_pi1 extends tslib_pibase {
 		if( $this->conf['terms.']['active'] == 1) {
 			// Extract subparts from the template
 			$subpart = $this->cObj->getSubpart($this->templateHtml, '###TERMS###');// Fill marker array
-			
+
 			// Set the URL to the terms
 			$markerArray['###TERMSANDCONDITIONSURL###'] = $this->cObj->getTypoLink_URL($this->conf['terms.']['params']);
-			
+
 			//render
 			$return = $this->cObj->substituteMarkerArrayCached($subpart, $markerArray);
-			
+
 			//send the html back
 			return($return);
 		} else {
-			//return empty 
+			//return empty
 			return ('');
 		}
-	
+
 	}
-	
-	
-	
+
+
+
 	/**
 	 * renders the options list for the countrie selector from static info table
 	 *
-	 *
-	 * @return	the countrie options
+	 * @return	the		countrie options
 	 */
 	function render_countries() {
 		/**
@@ -194,79 +212,78 @@ class tx_mzvoting_pi1 extends tslib_pibase {
 		if( $this->conf['country.']['active'] == 1) {
 			// Extract subparts from the template
 			$subpart = $this->cObj->getSubpart($this->templateHtml, '###COUNTRIESELECTOR###');
-			
+
 			$countries = $GLOBALS["TYPO3_DB"]->exec_SELECTgetRows("uid, cn_short_en", " static_countries", "1=1", "", "cn_short_en ASC");
-			
+
 			if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 1) {
 				t3lib_div::debug($countries);
 			}
-			
+
 			$countrie_options = '';
 			foreach($countries as $country) {
 				$countrie_options .= "\n" . '<option value="' . $country['uid'] . '">' . $country['cn_short_en'] . '</option>';
 				if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 					t3lib_div::debug($country);
 				}
-			}			
-						
-			
+			}
+
+
 			// Fill marker array
 			$markerArray['###OPTIONS###'] = $countrie_options;
-			
+
 			//render
 			$return = $this->cObj->substituteMarkerArrayCached($subpart, $markerArray);
-			
+
 			//send the html back
 			return($return);
-			
+
 		} else {
-			//return empty 
+			//return empty
 			return ('');
 		}
-	
+
 	}
-	
-	
+
+
 	/**
 	 * funktionsvorlage zum kopieren
 	 *
-	 * @param	array		$formdata: POST Data 
-	 *
-	 * @return	the output
+	 * @param	array		$formdata: POST Data
+	 * @return	the		output
 	 */
 	function formdata_handler($formdata) {
-		
+
 		if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 			t3lib_div::debug($formdata);
 		}
-		
-		
+
+
 		$return = '';
 		$errormsg = '';
 		$errors = 0;
-		
-		
-		
-		
+
+
+
+
 
 		if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 			t3lib_div::debug($this->conf['data.']['require.']);
 		}
-		
-		
+
+
 		// [TODO] IMPROVE VALIDATION
-		
+
 		foreach($this->conf['data.']['require.'] as $field => $setting) {
 			if($setting == 1 && empty($formdata[$field])) {
 				$errors ++;
 				$errormsg .= $this->conf['data.']['error.'][$field];
 			}
-			
-		}
-		
 
-		
-		
+		}
+
+
+
+
 		if($errors > 0 ) {
 			$return = $errormsg;
 			$return .= $this->get_votingform();
@@ -285,19 +302,19 @@ class tx_mzvoting_pi1 extends tslib_pibase {
 				$datatostore['terms'] = intval($formdata['terms']); unset($formdata['terms']);
 				unset($formdata['data']);
 				unset($formdata['submit']);
-				
-				
+
+
 				if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 					t3lib_div::debug($formdata);
 				}
-																  
-				$datatostore['pid'] = intval($this->conf['votestorage']);					  
-				$datatostore['tstamp'] = time();					  
-				$datatostore['crdate'] = time();					  
-				$datatostore['cruser_id'] = '0';					  
-				$datatostore['deleted'] = '0';					  
+
+				$datatostore['pid'] = intval($this->conf['votestorage']);
+				$datatostore['tstamp'] = time();
+				$datatostore['crdate'] = time();
+				$datatostore['cruser_id'] = '0';
+				$datatostore['deleted'] = '0';
 				$datatostore['hidden'] = '1';
-					
+
 				$datatostore['secret'] = md5(uniqid()) ;
 				$datatostore['vote_ip'] = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
 				$datatostore['vote_time'] = time() ;
@@ -305,95 +322,94 @@ class tx_mzvoting_pi1 extends tslib_pibase {
 				$datatostore['vote_browser'] = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
 				$datatostore['voting'] = intval($this->conf['voting']) ;
 				$datatostore['vote'] = isset($formdata['vote']) ? $formdata['vote'] : $this->conf['vote'];
-				
+
 				$formdata['host'] = gethostbyaddr($datatostore['vote_ip']);
-				
+
 				$datatostore['additional_data '] = serialize($formdata);
-				
+
 				if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 					t3lib_div::debug($datatostore);
 				}
-				
-				
+
+
 				$insert = $GLOBALS["TYPO3_DB"]->exec_INSERTquery 	('tx_mzvoting_votes',$datatostore );
 				if($insert) {
 					$vid = mysql_insert_id();
 					$return = "new row with uid $uid<br>".$insert;
-					
+
 					$this->send_confirmation_mail($datatostore['email'], $datatostore['firstname'], $datatostore['lastname'], $vid, $datatostore['secret']);
 				} else {
 					$return = "insert fialed";
 				}
-				
-				
-				
+
+
+
 			} else {
 				// fehler zeigen votet in the past 24h
 				// [todo] fehler
 				$next_time = $existing_records[0]['vote_time'] + (24*60*60);
 				$time_diff = $next_time - time();
-				
+
 				if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 					t3lib_div::debug($existing_records[0]['vote_time']);
 				}
-				
-				
+
+
 				if($time_diff > 0) {
 					// Extract subparts from the template
 					$subpart = $this->cObj->getSubpart($this->templateHtml, '###24HERROR###');// Fill marker array
-					
-					
+
+
 					$markerArray['###HOURS###'] = gmdate("H", $time_diff);
 					$markerArray['###MINUTES###'] = gmdate("i", $time_diff);
 					$markerArray['###SECONDS###'] = gmdate("s", $time_diff);
-					
+
 					//render
 					$return = $this->cObj->substituteMarkerArrayCached($subpart, $markerArray);
-					
+
 				} else {
 					//[TODO] resolution if time went to 0 during runtime so we never display 0 or less remaining blocktime
-					
-					
+
+
 					// Extract subparts from the template
 					$subpart = $this->cObj->getSubpart($this->templateHtml, '###24HERROR###');// Fill marker array
-					
-					
+
+
 					$markerArray['###HOURS###'] = gmdate("H", $time_diff);
 					$markerArray['###MINUTES###'] = gmdate("i", $time_diff);
 					$markerArray['###SECONDS###'] = gmdate("s", $time_diff);
-					
+
 					//render
 					$return = $this->cObj->substituteMarkerArrayCached($subpart, $markerArray);
-						
+
 				}
 			}
-			
+
 		}
-		
-		
+
+
 		return($return);
-	
+
 	}
-	
-	
+
+
 	/**
 	 * funktionsvorlage zum kopieren
 	 *
 	 * @param	string		$content: The PlugIn content
-	 *
-	 * @return	the voting form
+	 * @return	the		voting form
 	 */
 	function check_for_existing_votes($mail) {
-		
+
 		$decision_time = time()-(24*60*60);
 		$records = $GLOBALS["TYPO3_DB"]->exec_SELECTgetRows("uid, vote_time, hidden", "tx_mzvoting_votes", "vote_time > $decision_time AND email = '".trim($mail)."'", "", "vote_time DESC LIMIT 1");
-		
-		
+
+
 		return($records);
-	
+
 	}
-	
-	
+
+
 	/**
 	 * funktionsvorlage zum kopieren
 	 *
@@ -402,41 +418,40 @@ class tx_mzvoting_pi1 extends tslib_pibase {
 	 * @param	string		$lastname: receivers lastname
 	 * @param	integer		$id: voteid
 	 * @param	string		$secret: voteid
-	 *
-	 * @return	true or false
+	 * @return	true		or false
 	 */
 	function send_confirmation_mail($mail, $firstname, $lastname, $vid, $secret ) {
-		
+
 		$linkparams['tx_mzvoting']['vote']= 'con'.dechex($vid);
 		$linkparams['tx_mzvoting']['code']= $secret;
 		$linkparams['tx_mzvoting']['action']= '2'; //activate
-		
+
 		$url = 'http://' . $_SERVER['HTTP_HOST'] . '/' . $this->cObj->getTypoLink_URL($GLOBALS["TSFE"]->id,$linkparams);
-		
-		
+
+
 		if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 			t3lib_div::debug($this->cObj->getTypoLink_URL($GLOBALS["TSFE"]->id,$linkparams));
 		}
-		
+
 		// Extract subparts from the template
 		$html_subpart = $this->cObj->getSubpart($this->templateHtml, '###HTMLVOTINGCONFIRMATIONMAIL###');
 		$txt_subpart = $this->cObj->getSubpart($this->templateHtml, '###TXTVOTINGCONFIRMATIONMAIL###');
-		
+
 		// Fill marker array
-		$markerArray['###FIRSTNAME###'] = $firstname; 
-		$markerArray['###LASTNAME###'] = $lastname; 
-		$markerArray['###EMAIL###'] = $mail; 
-		$markerArray['###ACTIVATIONURL###']  =  $url; 
-		
+		$markerArray['###FIRSTNAME###'] = $firstname;
+		$markerArray['###LASTNAME###'] = $lastname;
+		$markerArray['###EMAIL###'] = $mail;
+		$markerArray['###ACTIVATIONURL###']  =  $url;
+
 		// Create the content by replacing the content markers in the template
 		$html_mailcontent = $this->cObj->substituteMarkerArrayCached($html_subpart, $markerArray);
 		$txt_mailcontent = $this->cObj->substituteMarkerArrayCached($txt_subpart, $markerArray);
-		
+
 		if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 			t3lib_div::debug($mailcontent);
 		}
-		
-		
+
+
 		$this->confirmationMail = t3lib_div::makeInstance('t3lib_htmlmail');
 		$this->confirmationMail->start();
 		$this->confirmationMail->recipient = $mail;
@@ -453,30 +468,29 @@ class tx_mzvoting_pi1 extends tslib_pibase {
 		$this->confirmationMail->addPlain($txt_mailcontent);
 		$this->confirmationMail->setHTML($this->confirmationMail->encodeMsg(/*$aEMailData['html_start'].*/$mailcontent/*.$aEMailData['html_end']*/));
 		$this->confirmationMail->send($mail);
-		
-		
+
+
 		if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 			t3lib_div::debug($this->confirmationMail);
 		}
-		
+
 		return('true');
-	
+
 	}
-	
-	
+
+
 	/**
 	 * funktionsvorlage zum kopieren
 	 *
 	 * @param	string		$content: The PlugIn content
-	 *
-	 * @return	the voting form
+	 * @return	the		voting form
 	 */
 	function confirmation_handler() {
 		//get the vars
 		$uid = intval(hexdec(substr($this->getVars['vote'],2)));
 		$secret = trim($this->getVars['code']);
-		
-		//check if entry exists		
+
+		//check if entry exists
 		$res = $GLOBALS["TYPO3_DB"]->exec_SELECTgetRows ("uid, hidden, confirm_time",
 													   "tx_mzvoting_votes",
 													   "uid = $uid AND secret = '$secret'"
@@ -488,46 +502,46 @@ class tx_mzvoting_pi1 extends tslib_pibase {
 				$error =  $this->report_error("3");
 			} elseif($res['hidden'] == 0) {
 				// ...but entry allready active
-				$error =  $this->report_error("4");				
+				$error =  $this->report_error("4");
 			} elseif($res['confirm_time'] > 0) {
 				// ...but entry has been deactivatet after confirmation
-				$error =  $this->report_error("5");				
+				$error =  $this->report_error("5");
 			} else {
 				// ... and it looks good
-					  
-				$datatostore['tstamp'] = time();				  
+
+				$datatostore['tstamp'] = time();
 				$datatostore['hidden'] = '0';
-					
+
 				$datatostore['confirm_ip'] = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
 				$datatostore['confirm_time'] = time() ;
 				$datatostore['confirm_referer'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
 				$datatostore['confirm_browser'] = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
-				
-				
+
+
 				if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 					t3lib_div::debug($datatostore);
 				}
-				
-				$update = $GLOBALS["TYPO3_DB"]->exec_UPDATEquery("tx_mzvoting_votes","deleted = 0 AND uid = $uid AND secret = '$secret' AND confirm_time = ''", $datatostore);  	
-				
+
+				$update = $GLOBALS["TYPO3_DB"]->exec_UPDATEquery("tx_mzvoting_votes","deleted = 0 AND uid = $uid AND secret = '$secret' AND confirm_time = ''", $datatostore);
+
 				if( $this->conf['debug.']['active'] == 1 && $this->conf['debug.']['level'] <= 2) {
 					t3lib_div::debug($update);
 				}
 				if($update) {
 					//update was sucessfull
-					
-					
+
+
 					// Extract subparts from the template
 					$subpart = $this->cObj->getSubpart($this->templateHtml, '###ACTIVATIONOK###');// Fill marker array
-					
+
 					//nomarkers yet
 					//$markerArray['###MARKER###'] = '';
-					
+
 					//render
 					$return = $this->cObj->substituteMarkerArrayCached($subpart, $markerArray);
-					
-					
-					
+
+
+
 					return($return);
 				} else {
 					//ooops the mysql update faild
@@ -538,49 +552,46 @@ class tx_mzvoting_pi1 extends tslib_pibase {
 			//ooops, no such entry here
 			$error =  $this->report_error("1");
 		}
-			
+
 		// Extract subparts from the template
 		$subpart = $this->cObj->getSubpart($this->templateHtml, '###ACTIVATIONERROR###');// Fill marker array
-		
+
 		//nomarkers jet
 		$markerArray['###ERROR###'] = $error;
-		
+
 		//render
 		$return = $this->cObj->substituteMarkerArrayCached($subpart, $markerArray);
-		
-		
-		
+
+
+
 		return($return);
-		
-	
+
+
 	}
-	
-	
+
+
 	/**
 	 * this function is called in case of an error
 	 * prepared for later functions
 	 *
 	 * @param	string		$errorcode: the errorcode
-	 *
 	 * @return	string		the redered error output
 	 */
 	function report_error($errorcode) {
-		
+
 		$return = $errorcode;
-		
+
 		return($return);
-	
+
 	}
-	
-	
+
+
 	   /**
-    * Init Function: here all the needed configuration values are stored in class variables
-
-    *
-    * @param    array   $conf: configuration array from TS
-    * @return   void
-
-    */
+ * Init Function: here all the needed configuration values are stored in class variables
+ *
+ * @param	array		$conf: configuration array from TS
+ * @return	void
+ */
    function load_config($conf) {
       $this->conf = $conf; // Store configuration
 
@@ -612,7 +623,7 @@ class tx_mzvoting_pi1 extends tslib_pibase {
             foreach ($sheet as $langName => $lang) {
                foreach(array_keys($lang) as $key) {
 
-                  $flexFormConf[$key] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 
+                  $flexFormConf[$key] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'],
                                                              $key, $sheetName, $langName);
 
                   if (!$flexFormConf[$key]) {
@@ -632,46 +643,45 @@ class tx_mzvoting_pi1 extends tslib_pibase {
       $this->conf = $conf;
 
    }
-	
-	
+
+
 	/**
 	 * displays the vote button
 	 *
 	 * @return	string		the html code
 	 */
 	function show_button() {
-			
+
 		// Extract subparts from the template
 		$subpart = $this->cObj->getSubpart($this->templateHtml, '###VOTEBUTTON###');// Fill marker array
-		
-		
+
+
 		$linkparams['tx_mzvoting']['action']= '1'; //vote
-		
-		
+
+
 		//nomarkers jet
 		$markerArray['###LINK###'] = $this->cObj->getTypoLink_URL($GLOBALS["TSFE"]->id,$linkparams);
-		
+
 		//render
 		$return = $this->cObj->substituteMarkerArrayCached($subpart, $markerArray);
-		
-		
-		
+
+
+
 		return($return);
-	
+
 	}
-	
-	
+
+
 	/**
 	 * funktionsvorlage zum kopieren
 	 *
 	 * @param	string		$content: The PlugIn content
-	 *
-	 * @return	the voting form
+	 * @return	the		voting form
 	 */
 	function test() {
-		
+
 		return($return);
-	
+
 	}
 }
 
